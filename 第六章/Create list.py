5289e848cfe4dc0f97ce12058af677ae56e1e73d
佳阳 李 <zhazha2457766502@outlook.com>
2022-07-09 15:05:52 +0800

    lnitial commit

--- 第六章/Create list.py
+++ 第六章/Create list.py
@@ -1,2 +1,9 @@
 # 第一种方法 使用【】
 list1 = ["hello", "world", 98, 86]
+"""第二种方法 内置函数list（）"""
+list2 = list(["hello", "world", 98])
+print(list1)
+print(list2)
+# 索引 从0开始 如果逆序 从-1开始 例如
+print(list1[0])
+print(list1[-4])
