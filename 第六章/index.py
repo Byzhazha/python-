2d87b0af90cffb5c6055e7b0acd8ce33c42406da
佳阳 李 <zhazha2457766502@outlook.com>
2022-07-09 15:29:34 +0800

    lnitial commit

---
+++ 第六章/index.py
@@ -0,0 +1,9 @@
+list1 = ["hello", "world", 98, "hello"]
+print(list1.index("hello"))           # 结果只为0 因为如查列表中存在n个相同元素，只返回第一个元素的索引
+# print(list1.index("hello",1,3))     # 不包括3
+# ValueError: 'hello' is not in list
+#   指定范围内查找索引
+print(list1.index("hello", 1, 4))
+
+
+
