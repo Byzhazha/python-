58daa7aedc52204365e51bb4bddc4a6d146737df
佳阳 李 <zhazha2457766502@outlook.com>
2022-07-09 10:20:47 +0800

    lnitial commit

--- 第五章/break and continue in nesting loop.py
+++ 第五章/break and continue in nesting loop.py
@@ -2,6 +2,8 @@
 for i in range(5):
     for j in range(1, 11):
         if j % 2 == 0:
-            break
-        print(j)
+            #break
+            continue
+        print(j, end="  ")
+    print()
 
