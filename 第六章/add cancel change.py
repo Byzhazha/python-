b6f9d9617e7defb5bb6afcf3f6c17f6edc4ec448
佳阳 李 <zhazha2457766502@outlook.com>
2022-07-10 16:55:21 +0800

    lnitial commit

--- 第六章/add cancel change.py
+++ 第六章/add cancel change.py
@@ -4,6 +4,8 @@ print("添加之前：", list1, id(list1))
 list1.append(100)
 print("添加后：", list1, id(list1))
 list2 = ["hello", "world"]
-list1.append(list2)
+# list1.append(list2)
 print(list1)                 # 将list2作为一个元素加给list1
+list1.extend(list2)           # 分别 将两个元素添加到末尾
+print(list1)
 
