55fdb1ae22460e1ba9a3e83643b0c2196937777a
佳阳 李 <zhazha2457766502@outlook.com>
2022-07-15 21:10:40 +0800

    lnitial commit

--- 第一章/fishC_game.py
+++ 第一章/fishC_game.py
@@ -1,7 +1,7 @@
 """  用pyhton设计第一个小游戏   """
-temp=input("不妨猜一下我心中想的数字：")
-guess=int(temp)
-if guess==8:
+temp = input("不妨猜一下我心中想的数字：")
+guess = int(temp)
+if guess == 8:
     print("你是我心里的蛔虫吗？")
     print("哼！猜中了也没有奖励！")
 else:
