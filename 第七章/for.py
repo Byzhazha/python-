ab489a4e3f4941b386edb3a47b942003ad04e5a6
佳阳 李 <zhazha2457766502@outlook.com>
2022-07-16 15:58:22 +0800

    Initial commit

---
+++ 第七章/for.py
@@ -0,0 +1,4 @@
+abcd = {"张三": 100, "李四": 98, "王五":45}
+# 字典元素的遍历
+for items in abcd:
+    print(items, abcd[items], abcd.get(items))
--- 第七章/get view.py
+++ 第七章/get view.py
@@ -1,1 +1,13 @@
 abcd = {"张三": 100, "李四": 98, "王五":45}
+keys = abcd.keys()
+print(keys,type(keys))
+print(list(keys))       # 将所有的key组成的视图转成列表
+
+# 获取所有的值
+values = abcd.values()
+print(values,type(values))
+
+# 获取所有键值对
+items = abcd.items()
+print(items)
+print(list(items))   # 转换之后的列表元素是由元组组成   后续会讲到
