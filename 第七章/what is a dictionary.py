47121d05262900e6d3c245334f5b85a0b26228a4
佳阳 李 <zhazha2457766502@outlook.com>
2022-07-16 08:48:04 +0800

    lnitial commit

---
+++ 第七章/what is a dictionary.py
@@ -0,0 +1,11 @@
+# 字典的创建方式
+abcd = {"张三": 100, "李四": 98, "王五":45}         # :左边为键 key  右边为值  value
+print(abcd, type(abcd))
+
+# 第二种创建方式 dict 函数
+student = dict(name="jack", age=20)
+print(student,type(student))
+
+"""空字典"""
+d = {}
+print(d)
