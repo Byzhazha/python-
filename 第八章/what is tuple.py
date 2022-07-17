074e668179d58ce374a97c8ac7724b6e15c09089
佳阳 李 <zhazha2457766502@outlook.com>
2022-07-17 09:08:48 +0800

    Initial commit

--- 第八章/what is tuple.py
+++ 第八章/what is tuple.py
@@ -1,1 +1,13 @@
 """可变序列 不可变序列"""
+"""可变序列： 列表，字典"""
+lis = [10, 20, 45]
+print(id(lis))
+lis.append(300)
+print(id(lis))
+# 不可变序列 字符串 元组 没有增删改操作
+s = "hello"
+print(id(s))
+s = s+"world"
+print(s,id(s))       # id 发生变化
+
+
