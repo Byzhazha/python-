a693df505950aae2098d635e593e13ecaecdbdec
佳阳 李 <zhazha2457766502@outlook.com>
2022-07-19 16:46:51 +0800

    Initial commit

---
+++ 第九章/align.py
@@ -0,0 +1,14 @@
+s = "hello,python"
+"""居中对齐"""
+print(s.center(20,"*"))
+"""左对齐"""
+print(s.ljust(20,"*"))
+print(s.ljust(10))                    # 小于实际长度，则返回原字符
+print(s.ljust(20))
+"""右对齐"""
+print(s.rjust(20,"*"))
+print(s.rjust(10))
+print(s.rjust(20))
+
+# 右对齐 使用0填充
+print(s.zfill(20))
