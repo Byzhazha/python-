3d73f407b645b07dc9749e156356dcf01001d983
佳阳 李 <zhazha2457766502@outlook.com>
2022-07-21 22:00:04 +0800

    Initial commit

--- 第十章/function.py
+++ 第十章/function.py
@@ -3,6 +3,7 @@ def calc(a,b):
     return sum1
 
 
-result=calc(10,20)
-print(result)
+result=calc(10,20)                   # result=calc(b=100,a=20)
+print(result)                        # print(result)          此方法也可行
+
 
