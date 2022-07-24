83565176d72956814724bbf1b3455f5008ceb5ee
佳阳 李 <zhazha2457766502@outlook.com>
2022-07-24 16:20:20 +0800

    Initial commit

---
+++ BUG/Common errors.py
@@ -0,0 +1,13 @@
+"""
+1.除或取余0      print(10/0)    ZeroDivisionError
+2.序列中没有此索引
+list1=[1,2,3]    print(list1[4])     IndexError
+3.映射中没有这个键
+dict1={"li":156}     print(dict1[1532132])   KeyError
+4.未声明/初始化对象
+while a<10:          print(a)            NameError
+5.语法错误    int a =10              SyntaxError
+6.传入无效的参数    a=int("hello)      valueError
+"""
+
+
