7c8396449f99673713fb60ff384067c2e4ad73ce
佳阳 李 <zhazha2457766502@outlook.com>
2022-07-29 10:26:21 +0800

    Initial commit

---
+++ 第十二章/Standard library.py
@@ -0,0 +1,6 @@
+from random import randint
+from random import choice
+print(randint(1, 60))
+players = [1, 2, 3, 4, 5, 6, "sd"]            # 它将一个列表或元组作为参数，并随机返回其中的一个元素
+first_up = choice(players)
+print(first_up)
