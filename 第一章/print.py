0cdce1f89444567304918818080ec61de363fd96
佳阳 李 <zhazha2457766502@outlook.com>
2022-08-06 10:23:17 +0800

    Initial commit

--- 第一章/print.py
+++ 第一章/print.py
@@ -1,4 +1,4 @@
-#写入文件夹
-wk=open('D:/text.txt','a+')        #    如果文件不存在就创建，如果存在，就在内容后面继续追加
+#     写入文件夹
+wk=open('D:/text.txt','a+')               #             如果文件不存在就创建，如果存在，就在内容后面继续追加
 print('hello world',file=wk)
 wk.close()
---
+++ 第十四章/File reading and writing.py
@@ -0,0 +1,23 @@
+file = open("1.txt", "r")
+print(file.read())
+file.close()          # 文件需是ansi格式  也就是GBK
+
+file =open("2.txt", "w")
+file.write("python")
+file.close()
+
+file =open("2.txt", "a")
+file.write("p5415656")
+file.close()
+
+src_file =open("1.jpg", "rb")      #  二进制文件
+target_file = open("copy1.jpg", "wb")
+print(target_file.write(src_file.read()))
+target_file.close()
+src_file.close()
+
+file1=open('2.txt', 'a+')               #             如果文件不存在就创建，如果存在，就在内容后面继续追加
+print('hello world', file=file1)
+file1.close()       # 以读写的方式打开文件，不能单独使用，需要与其他模式一起使用  例如 a+
+
+
