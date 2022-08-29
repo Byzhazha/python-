840bf70dd0f958e87c7b84c6776b41398b89f2c1
佳阳 李 <zhazha2457766502@outlook.com>
2022-08-29 19:42:38 +0800

    Initial commit

---
+++ 第十四章/Student system.py
@@ -0,0 +1,41 @@
+def main():
+    while True:
+        menu()
+        choice = int(input("请选择"))
+        if choice in [0, 1, 2, 3, 4, 5, 6, 7]:
+            if choice == 0:
+                answer = input("您确定要退出系统吗？y/n")
+                if answer == 'y' or answer == 'Y':
+                    print("谢谢您的使用")
+                    break  # 退出系统
+                else:
+                    continue
+            elif choice == 1:
+                insert()  # 录入学生信息
+            elif choice == 2:
+                search()
+            elif choice == 3:
+                pass
+            elif choice == 4:
+                pass
+            elif choice == 5:
+                pass
+            elif choice == 6:
+                pass
+            elif choice == 7:
+                pass
+            elif choice == 7:
+                pass
+
+
+def menu():
+    print("========================学生管理系统=========================")
+    print("------------------------功能菜单----------------------------")
+    print("\t\t\t\t\t\t1.录入学生信息")
+    print("\t\t\t\t\t\t2.查找学生信息")
+    print("\t\t\t\t\t\t3.删除学生信息")
+    print("\t\t\t\t\t\t4.修改学生信息")
+    print("\t\t\t\t\t\t5.排序")
+    print("\t\t\t\t\t\t6.统计学生总人数")
+    print("\t\t\t\t\t\t7.显示所有学生信息")
+    print("\t\t\t\t\t\t0.退出")
